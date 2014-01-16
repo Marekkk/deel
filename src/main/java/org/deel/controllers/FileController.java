b058f47401899b50c42d0bf6bdd4d2ec91fca0d8
Marco <marcopecos+electtricsheep@gmail.com>
2014-01-16 12:45:24 +0100

    fix json

--- src/main/java/org/deel/controllers/FileController.java
+++ src/main/java/org/deel/controllers/FileController.java
@@ -2,14 +2,14 @@ package org.deel.controllers;
 
 import java.io.IOException;
 import java.security.Principal;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.deel.domain.User;
 import org.deel.service.FileService;
 import org.deel.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,9 +40,18 @@ public class FileController {
 	public void setFileService(FileService fileService) {
 		this.fileService = fileService;
 	}
+	
+	@RequestMapping("/test")
+	public @ResponseBody void test(Principal principal, ModelMap map){
+	
+		Map<String,Object> m = new HashMap<String, Object>();
+		m.put("test", "asd");
+		map.addAttribute("test", "asd");
+		return ;
+	}
 
 	@RequestMapping(value = "/file/upload", method = RequestMethod.POST)
-	public @ResponseBody ModelMap fileUploadJSON(FileForm files, Principal principal, ModelMap model) {
+	public @ResponseBody Map<String, Object> fileUploadJSON(FileForm files, Principal principal, ModelMap model) {
 		/* TODO fix security.getPrincipal return our UserClass */
 //		//Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 //		String username;
@@ -54,7 +63,8 @@ public class FileController {
 		
 		//String username = principal.getName();
 		
-		User curr = userService.findUserByUsername("nick"); 
+		User curr = userService.findUserByUsername("nick");
+		Map<String, Object> jsonReturn = new HashMap<String, Object>();
 		 
 		List<MultipartFile> mFiles = files.getFiles();
 		
@@ -65,18 +75,18 @@ public class FileController {
 						files.getPath(), 
 						multipartFile.getBytes());
 				
-				model.addAttribute(multipartFile.getOriginalFilename(), 
+				jsonReturn.put(multipartFile.getOriginalFilename(), 
 						"success");
 				
 			} catch (IOException e) {
-				model.addAttribute(multipartFile.getOriginalFilename(), 
+				jsonReturn.put(multipartFile.getOriginalFilename(), 
 						"failed");
 				e.printStackTrace();
 			}
 		}
 
 		/* TODO real message codes */
-		return model;
+		return jsonReturn;
 	}
 }
 
