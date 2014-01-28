83addeeb063456a298cca16ae6493356218a9c1c
veget88 <pierpaolo.rametta@gmail.com>
2014-01-28 18:33:24 +0100

    Adding autocomplete to sharing file

---
+++ src/main/java/org/deel/domain/UserInfo.java
@@ -0,0 +1,34 @@
+package org.deel.domain;
+
+public class UserInfo {
+	private String username;
+	private String name;
+	private Long id;
+	
+	public UserInfo(User u) {
+		this.username = u.getUsername();
+		this.id = u.getId();
+		this.name = u.getName();
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	
+}	
