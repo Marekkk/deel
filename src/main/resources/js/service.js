207b474712b5ae4274336a69059848fb38e56131
veget88 <pierpaolo.rametta@gmail.com>
2014-01-29 17:40:04 +0100

    Relation folder added

--- src/main/resources/js/service.js
+++ src/main/resources/js/service.js
@@ -72,75 +72,7 @@ var service = (function ($) {
 			$.when(
 				$.get("team/list"),
 				$.get("user/list")
-			).done(function (teamXHR, userXHR) {
-				teams = teamXHR[0].teams;
-				users = userXHR[0].users;
-				
-				var sharing = $("#sharingList");
-				var insertUser = $("<input></input>");
-				insertUser.attr("id", "inputSharing");
-				var sharingWith = $("<div></div");
-				sharing.append(sharingWith);
-				sharingWith.attr("id", "sharingWith");
-				sharingWith.before(insertUser);
-				var sender = $("<button>Share!</button>");
-				sender.attr("id", "sender");
-				sharingWith.append(sender);
-				var usersSharing = new Array();
-				var teamsSharing = new Array();
-				
-				users.forEach(function (u) {
-					u.label = u.username;
-					u.type = "user";
-				});
-				
-				teams.forEach(function (t) {
-					t.label = t.name;
-					t.type = "team";
-				});
-				
-				insertUser.autocomplete({
-					source : users.concat(teams),
-					select : function(e, ui) {
-						if (ui.item.type == "user")
-							usersSharing.push(ui.item.id);
-						else if (ui.item.type == "team")
-							teamsSharing.push(ui.item.id);
-			
-						var sharingWith = $("#sharingWith");
-						var p = $("<p></p>");
-						p.html(ui.item.label);
-						sharingWith.append(p);
-						$("#inputSharing").val("");
-						return false;
-					}
-				});
-					
-				sender.click(function() {
-					
-					var message = new Object();
-
-					message.users = usersSharing;
-					message.teams = teamsSharing;
-					message.file = fp.id;
-					
-					console.log(message);
-
-					$.ajax({
-						url : "file/share",
-						type : 'POST',
-						data : JSON.stringify(message),
-						dataType : "json",
-						contentType : "application/json",
-						success : function(returndata) {
-							alert("File Shared!");
-						}
-					});
-
-				});
-			});
-			
-			$('#sharingList').dialog("open");
+			).done(function (a,b){cb(fp,a,b)});
 		},
 		
 		newFolder : function (folderName, currentFolder, cb) {
